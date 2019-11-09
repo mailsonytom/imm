3797c2cb4ce89227dc1849f3727146c377b3f5d8
Tony Tom <ttom@perleybrook.com>
2019-11-09 12:18:20 +0530

    add student validation added

--- admin/addstudents.php
+++ admin/addstudents.php
@@ -5,6 +5,13 @@ if (!isset($_SESSION['admin'])) {
                 window.location = "signin.php"
                  </script>';
 }
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 if (!isset($_SESSION['course']) || $_SESSION['course'] < 1) {
     echo '<script type="text/javascript">
     window.location = "selectcourse.php"
@@ -13,20 +20,27 @@ if (!isset($_SESSION['course']) || $_SESSION['course'] < 1) {
     $course_id = $_SESSION['course'];
     $name = $admno = $father = $mother = $address = $phone = $error = "";
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (empty($_POST['name']) || empty($_POST['admno']) || empty($_POST['father']) || empty($_POST['mother']) || empty($_POST['address']) || empty($_POST['phone']) || empty($_POST['email'])) {
-            $error = "Please fill out all the fields";
+        $flag = 0;
+        if (empty($_POST["name"])) {
+            $error = "Name is required";
+            $flag = 1;
         } else {
-            $flag = 0;
-            $name = $conn->real_escape_string($_POST['name']);
-            $admno = $conn->real_escape_string($_POST['admno']);
-            $father = $conn->real_escape_string($_POST['father']);
-            $mother = $conn->real_escape_string($_POST['mother']);
-            $address = $conn->real_escape_string($_POST['address']);
-            $phone = $conn->real_escape_string($_POST['phone']);
-            $email = $conn->real_escape_string($_POST['email']);
-            $parent_pass = password_hash($_POST['phone'], PASSWORD_DEFAULT);
-            $sql = "INSERT INTO students (name, admn_no, father_name, mother_name, address, phone, email, course_id, sem_id, parent_pass)
-            VALUES ('$name', '$admno', '$father', '$mother', '$address', '$phone', '$email', '$course_id', '1', '$parent_pass')";
+            $name = test_input($_POST['name']);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+                $flag = 1;
+                $error = "Only letters and white space allowed";
+            }
+        }
+        if (empty($_POST["admno"])) {
+            $error = "Admission number is required";
+            $flag = 1;
+        } else {
+            $admno = test_input($_POST['admno']);
+            if (!preg_match("/^[1-9][0-9]*$/", $admno)) {
+                $flag = 1;
+                $error = "Invalid admission number";
+            }
             $select_query = "SELECT admn_no FROM students";
             $result = mysqli_query($conn, $select_query);
             while ($row = mysqli_fetch_assoc($result)) {
@@ -35,21 +49,76 @@ if (!isset($_SESSION['course']) || $_SESSION['course'] < 1) {
                     $flag = 1;
                 }
             }
-            if ($flag == 0) {
-                if (mysqli_query($conn, $sql)) {
-                    $error = $name . " entered successfully into records";
-                    $student_id = mysqli_fetch_assoc(mysqli_query($conn, "SELECT id from students WHERE admn_no='$admno'"))['id'];
-                    $result = mysqli_query($conn, "SELECT id from subject WHERE sem_id=1");
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $marksql = "INSERT INTO marks (student_id, subject_id, marks) VALUES ('$student_id', '" . $row['id'] . "', 0)";
-                        mysqli_query($conn, $marksql);
-                    }
-                } else {
-                    $error = "Something failed. Please try again.";
+        }
+        if (empty($_POST["father"])) {
+            $error = "Father's name is required";
+            $flag = 1;
+        } else {
+            $father = test_input($_POST['father']);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/", $father)) {
+                $flag = 1;
+                $error = "Only letters and white space allowed";
+            }
+        }
+        if (empty($_POST["mother"])) {
+            $error = "Mother's name is required";
+            $flag = 1;
+        } else {
+            $mother = test_input($_POST['mother']);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/", $mother)) {
+                $flag = 1;
+                $error = "Only letters and white space allowed";
+            }
+        }
+        if (empty($_POST["address"])) {
+            $error = "Address is required";
+            $flag = 1;
+        } else {
+            $address = test_input($conn->real_escape_string($_POST['address']));
+            // check if name only contains letters and whitespace
+        }
+        if (empty($_POST["phone"])) {
+            $error = "Phone number is required";
+            $flag = 1;
+        } else {
+            $phone = test_input($_POST['phone']);
+            $parent_pass = password_hash($_POST['phone'], PASSWORD_DEFAULT);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[1-9][0-9]{9}$/", $phone)) {
+                $flag = 1;
+                $error = "Wrong phone number format";
+            }
+        }
+        if (empty($_POST["email"])) {
+            $error = "Email is required";
+            $flag = 1;
+        } else {
+            $email = test_input($_POST['email']);
+            // check if name only contains letters and whitespace
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL, $email)) {
+                $flag = 1;
+                $error = "Wrong email format";
+            }
+        }
+        if ($flag == 0) {
+            $sql = "INSERT INTO students (name, admn_no, father_name, mother_name, address, phone, email, course_id, sem_id, parent_pass)
+            VALUES ('$name', '$admno', '$father', '$mother', '$address', '$phone', '$email', '$course_id', '1', '$parent_pass')";
+            if (mysqli_query($conn, $sql)) {
+                $error = $name . " entered successfully into records";
+                $student_id = mysqli_fetch_assoc(mysqli_query($conn, "SELECT id from students WHERE admn_no='$admno'"))['id'];
+                $result = mysqli_query($conn, "SELECT id from subject WHERE sem_id=1");
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $marksql = "INSERT INTO marks (student_id, subject_id, marks) VALUES ('$student_id', '" . $row['id'] . "', 0)";
+                    mysqli_query($conn, $marksql);
                 }
+            } else {
+                $error = "Something failed. Please try again.";
             }
         }
     }
+
     ?>
     <html>
     <title>Internal Mark Management</title>
